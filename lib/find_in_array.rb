6f80c2abcb7df4290b749219f259e44a832b6a8b
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-02 14:44:02 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  for i in array
+    index = nil
+    if i == value_to_find
+      index = array.index(i)
+      break
+    end
+  end
+  
+  index
+  
 end
